8efc479f890e8b55c79627786a36b07a888ba882
홍영기 <siesto@webworker.local>
2021-06-16 10:24:58 +0900

    7

--- config/urls.py
+++ config/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     #     comment_views.CommentWriteView.as_view(),
     #     name="create",
     # ),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
