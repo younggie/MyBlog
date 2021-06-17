5df0244a94565aa130fb2215d9ac67794b88a8b8
홍영기 <siesto@webworker.local>
2021-06-17 11:46:49 +0900

    on_likes exeption

--- likes/templatetags/on_likes.py
+++ likes/templatetags/on_likes.py
@@ -1,5 +1,6 @@
 from django import template
 from likes import models as like_models
+from posts import models as post_models
 
 register = template.Library()
 
@@ -7,6 +8,12 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def on_likes(context, post):
     user = context.request.user
+    if user:
+        try:
+            the_list = like_models.Like.objects.get(user=user, name="My likes")
+            return post in the_list.posts.all()
+        except   like_models.Like.DoesNotExist:
+            return  False
     
-    the_list = like_models.Like.objects.get(user=user, name="My likes")
-    return post in the_list.posts.all()
+
+    
